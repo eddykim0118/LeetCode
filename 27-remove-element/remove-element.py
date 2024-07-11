0fedf3b19a84a31ff151f4e2372a10591ff1a288
Eddy Kim <eddykim0118@gmail.com>
2024-07-11 16:45:59 -0600

    Time: 31 ms (90.69%) | Memory: 16.5 MB (68.94%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -13,11 +13,11 @@ class Solution:
 
 solution = Solution()
 
-nums1 = [3, 2, 2, 3]
-val1 = 3
-result1 = solution.removeElement(nums1, val1)
+# nums1 = [3, 2, 2, 3]
+# val1 = 3
+# result1 = solution.removeElement(nums1, val1)
 
-nums2 = [0, 1,2,2,3,0,4,2]
-val2 = 2
-results2 = solution.removeElement(nums2, val2)
+# nums2 = [0, 1,2,2,3,0,4,2]
+# val2 = 2
+# results2 = solution.removeElement(nums2, val2)
         
