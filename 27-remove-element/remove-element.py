9ede14183db79f11d3654871a4bf9a77c6f66c8a
Eddy Kim <eddykim0118@gmail.com>
2024-07-18 11:10:42 -0600

    Time: 33 ms (82.05%) | Memory: 16.4 MB (95.31%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,23 +1,10 @@
-from typing import List
-
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        k = 0
-
+        k = 0  # Initialize the count of elements not equal to val
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]
                 k += 1
-
         return k
 
-solution = Solution()
-
-# nums1 = [3, 2, 2, 3]
-# val1 = 3
-# result1 = solution.removeElement(nums1, val1)
-
-# nums2 = [0, 1,2,2,3,0,4,2]
-# val2 = 2
-# results2 = solution.removeElement(nums2, val2)
         
