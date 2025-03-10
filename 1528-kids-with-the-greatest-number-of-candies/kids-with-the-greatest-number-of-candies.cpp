090a2ea1155e07846cd00de2ae4edf4bb9304776
Eddy Kim <eddykim0118@gmail.com>
2025-03-09 20:22:58 -0600

    Time: 0 ms (100.00%) | Memory: 12.4 MB (91.72%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,15 @@
+using namespace std;
+
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int most = *max_element(candies.begin(), candies.end());
+        vector<bool> result;
+
+        for (int candy : candies) {
+            result.push_back(candy + extraCandies >= most);
+        }
+
+        return result;
+    }
+};
